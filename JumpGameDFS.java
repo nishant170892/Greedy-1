f5e276b68df4e9afc642030155d8d385192fc9d3
nishant170892 <160679621+nishant170892@users.noreply.github.com>
2024-05-11 01:43:33 -0400

    Create JumpGameDFS.java

---
+++ JumpGameDFS.java
@@ -0,0 +1,27 @@
+// Time Complexity : O(n) 
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes, https://leetcode.com/problems/jump-game/
+// Any problem you faced while coding this : No
+
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums==null || nums.length<2) return true;
+          HashSet<Integer> set = new HashSet<>();
+          return dfs(nums,0,set);
+    }
+
+    private Boolean dfs(int[] nums, int idx, HashSet<Integer>set){
+         //base
+         if(idx==nums.length-1) return true;
+
+         //logic
+         set.add(idx);
+         for(int j=1;j<=nums[idx];j++){
+            int newIdx = idx+j;
+            if(set.contains(newIdx)) continue;
+            if(dfs(nums,newIdx,set)) return true;
+         }
+         return false;
+    }
+}
